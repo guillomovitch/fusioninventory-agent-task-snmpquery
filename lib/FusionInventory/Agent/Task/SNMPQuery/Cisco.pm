c98e4b2e2ac2d1384e07defc9277c0a056165319
Guillaume Rousse <Guillaume.Rousse@inria.fr>
2010-10-09 16:55:21 +0200

    move common function at the beginning of the module, for easier comparaison

--- lib/FusionInventory/Agent/Task/SNMPQuery/Cisco.pm
+++ lib/FusionInventory/Agent/Task/SNMPQuery/Cisco.pm
@@ -3,60 +3,6 @@ package FusionInventory::Agent::Task::SNMPQuery::Cisco;
 use strict;
 use warnings;
 
-sub TrunkPorts {
-    my ($data, $device, $index) = @_;
-
-    while ( (my $port_id, my $trunk) = each (%{$data->{vlanTrunkPortDynamicStatus}}) ) {
-        if ($trunk eq "1") {
-            $device->{PORTS}->{PORT}->[$index->{lastSplitObject($port_id)}]->{TRUNK} = $trunk;
-        } else {
-            $device->{PORTS}->{PORT}->[$index->{lastSplitObject($port_id)}]->{TRUNK} = '0';
-        }
-        delete $data->{vlanTrunkPortDynamicStatus}->{$port_id};
-    }
-    if (keys (%{$data->{vlanTrunkPortDynamicStatus}}) eq "0") {
-        delete $data->{vlanTrunkPortDynamicStatus};
-    }
-}
-
-sub CDPPorts {
-    my ($data, $device, $walk, $index) = @_;
-
-    my $short_number;
-
-    if (ref($data->{cdpCacheAddress}) eq "HASH"){
-        while ( my ( $number, $ip_hex) = each (%{$data->{cdpCacheAddress}}) ) {
-            $ip_hex =~ s/://g;
-            $short_number = $number;
-            $short_number =~ s/$walk->{cdpCacheAddress}->{OID}//;
-            my @array = split(/\./, $short_number);
-            my @ip_num = split(/(\S{2})/, $ip_hex);
-            my $ip = (hex $ip_num[3]).".".(hex $ip_num[5]).".".(hex $ip_num[7]).".".(hex $ip_num[9]);
-            if ($ip ne "0.0.0.0") {
-                $device->{PORTS}->{PORT}->[$index->{$array[1]}]->{CONNECTIONS}->{CONNECTION}->{IP} = $ip;
-                $device->{PORTS}->{PORT}->[$index->{$array[1]}]->{CONNECTIONS}->{CDP} = "1";
-                if (defined($data->{cdpCacheDevicePort}->{$walk->{cdpCacheDevicePort}->{OID}.$short_number})) {
-                    $device->{PORTS}->{PORT}->[$index->{$array[1]}]->{CONNECTIONS}->{CONNECTION}->{IFDESCR} = $data->{cdpCacheDevicePort}->{$walk->{cdpCacheDevicePort}->{OID}.$short_number};
-                }
-            }
-            delete $data->{cdpCacheAddress}->{$number};
-            if (ref($data->{cdpCacheDevicePort}) eq "HASH"){
-                delete $data->{cdpCacheDevicePort}->{$number};
-            }
-        }
-        if (keys (%{$data->{cdpCacheAddress}}) eq "0") {
-            delete $data->{cdpCacheAddress};
-        }
-        if (ref($data->{cdpCacheDevicePort}) eq "HASH"){
-            if (keys (%{$data->{cdpCacheDevicePort}}) eq "0") {
-                delete $data->{cdpCacheDevicePort};
-            }
-        }
-    }
-}
-
-
-
 sub GetMAC {
     my ($data, $device, $vlan_id, $index, $walk) = @_;
 
@@ -108,7 +54,57 @@ sub GetMAC {
     }
 }
 
+sub TrunkPorts {
+    my ($data, $device, $index) = @_;
 
+    while ( (my $port_id, my $trunk) = each (%{$data->{vlanTrunkPortDynamicStatus}}) ) {
+        if ($trunk eq "1") {
+            $device->{PORTS}->{PORT}->[$index->{lastSplitObject($port_id)}]->{TRUNK} = $trunk;
+        } else {
+            $device->{PORTS}->{PORT}->[$index->{lastSplitObject($port_id)}]->{TRUNK} = '0';
+        }
+        delete $data->{vlanTrunkPortDynamicStatus}->{$port_id};
+    }
+    if (keys (%{$data->{vlanTrunkPortDynamicStatus}}) eq "0") {
+        delete $data->{vlanTrunkPortDynamicStatus};
+    }
+}
+
+sub CDPPorts {
+    my ($data, $device, $walk, $index) = @_;
+
+    my $short_number;
+
+    if (ref($data->{cdpCacheAddress}) eq "HASH"){
+        while ( my ( $number, $ip_hex) = each (%{$data->{cdpCacheAddress}}) ) {
+            $ip_hex =~ s/://g;
+            $short_number = $number;
+            $short_number =~ s/$walk->{cdpCacheAddress}->{OID}//;
+            my @array = split(/\./, $short_number);
+            my @ip_num = split(/(\S{2})/, $ip_hex);
+            my $ip = (hex $ip_num[3]).".".(hex $ip_num[5]).".".(hex $ip_num[7]).".".(hex $ip_num[9]);
+            if ($ip ne "0.0.0.0") {
+                $device->{PORTS}->{PORT}->[$index->{$array[1]}]->{CONNECTIONS}->{CONNECTION}->{IP} = $ip;
+                $device->{PORTS}->{PORT}->[$index->{$array[1]}]->{CONNECTIONS}->{CDP} = "1";
+                if (defined($data->{cdpCacheDevicePort}->{$walk->{cdpCacheDevicePort}->{OID}.$short_number})) {
+                    $device->{PORTS}->{PORT}->[$index->{$array[1]}]->{CONNECTIONS}->{CONNECTION}->{IFDESCR} = $data->{cdpCacheDevicePort}->{$walk->{cdpCacheDevicePort}->{OID}.$short_number};
+                }
+            }
+            delete $data->{cdpCacheAddress}->{$number};
+            if (ref($data->{cdpCacheDevicePort}) eq "HASH"){
+                delete $data->{cdpCacheDevicePort}->{$number};
+            }
+        }
+        if (keys (%{$data->{cdpCacheAddress}}) eq "0") {
+            delete $data->{cdpCacheAddress};
+        }
+        if (ref($data->{cdpCacheDevicePort}) eq "HASH"){
+            if (keys (%{$data->{cdpCacheDevicePort}}) eq "0") {
+                delete $data->{cdpCacheDevicePort};
+            }
+        }
+    }
+}
 
 sub lastSplitObject {
     my $var = shift;
